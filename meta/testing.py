21964a166242998a0a2cd8620e4ea0753e5158f2
vatsan <vatsan.cs@gmail.com>
2013-10-05 13:50:07 -0700

    Fixed version checks - Python 2.x does not have major attribute in sys.version_info

--- meta/testing.py
+++ meta/testing.py
@@ -7,7 +7,7 @@ Created on Nov 5, 2011
 import sys
 import unittest
 
-py2 = sys.version_info.major < 3
+py2 = sys.version_info[0] < 3 or sys.version_info.major < 3
 py3 = not py2
 
 py2only = unittest.skipIf(not py2, "Only valid for python 2.x")
